7a80376f50bbfbd27e85afc039906ca5c8d362f8
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-12-25 14:09:14 +0600

    Linked List for DSA

--- Linked List/linked_list.cpp
+++ Linked List/linked_list.cpp
@@ -9,28 +9,28 @@ int main() {
         cout<<"\nMAIN MENU - SELECT PROBLEM\n";
         cout<<"\n===========================\n";
         cout<<" 0. Stack Operations\n";
-        cout<<" 00. Queue Operations\n";
+        cout<<" -1. Queue Operations\n";
         cout<<" 1. Student Struct\n";
         cout<<" 2. Point Distance\n";
         cout<<" 3. Pointer Access\n";
         cout<<" 4. Product Array\n";
         cout<<" 5. Rectangle Area\n";
-        cout << "6. Time Addition\n";
-        cout << "7. Increment via Pointer\n";
-        cout << "8. NULL Pointer\n";
-        cout << "9. Single Pointer Multiple Nodes\n";
-        cout << "10. Linked Nodes\n";
-        cout << "11. Exit Program\n";
-        cout << "Enter choice: ";
+        cout<<" 6. Time Addition\n";
+        cout<<" 7. Increment via Pointer\n";
+        cout<<" 8. NULL Pointer\n";
+        cout<<" 9. Single Pointer Multiple Nodes\n";
+        cout<<" 10. Linked Nodes\n";
+        cout<<" 11. Exit Program\n";
+        cout<<" Enter choice: ";
         cin >> choice;
 
         switch (choice) {
             case 0:
                 stackMenu();
                 break;
-            // case 00:
-            //     queueMenu();
-            //     break;
+            case -1:
+                queueMenu();
+                break;
             case 1:
                 problem1();
                 break;
